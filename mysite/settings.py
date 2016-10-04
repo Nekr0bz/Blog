d0bd46ec555a2a1bcb8811c39ace67e8f90f20d2
Nekr0z <Nekr0bz@yandex.ru>
2016-10-04 13:28:30 +0300

    Первоначальные настройки проекта

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'templateBD',
+        'USER': 'root',
+        'PASSWORD': '1234'
     }
 }
 
@@ -103,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-RU'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
