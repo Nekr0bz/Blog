534c9984a593382fd716a1017f60b3caf2b86964
Nekr0z <Nekr0bz@yandex.ru>
2016-10-04 15:51:36 +0300

    Настрока интерфейса администратора для работы с Artcile

--- article/admin.py
+++ article/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Article, Comments
 
-# Register your models here.
+
+class CommentsInline (admin.TabularInline):
+    model = Comments
+    extra = 2
+    readonly_fields = ['comments_likes', 'comments_dislikes']
+
+class ArticleAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,                          {'fields': ['article_title']}),
+        (None,                          {'fields': ['article_text']}),
+        (None,                          {'fields': ['article_datetime']}),
+        ('Оценка пользователей',        {'fields': [('article_likes', 'article_dislikes')], 'classes':['collapse']})
+    ]
+    readonly_fields = ['article_likes', 'article_dislikes']
+    inlines = [CommentsInline]
+
+    list_display = ['__str__', 'article_datetime', 'article_likes', 'article_dislikes']
+    list_filter = ['article_datetime']
+    search_fields = ['article_title']
+    list_per_page = 25
+
+
+admin.site.register(Article, ArticleAdmin)
--- article/models.py
+++ article/models.py
@@ -1,4 +1,3 @@
-import datetime
 from django.utils import timezone
 from django.db import models
 
@@ -6,25 +5,26 @@ class Article (models.Model):
     class Meta:
         db_table = 'article'
 
-    article_title = models.CharField(max_length=200)
-    article_text = models.TextField()
-    article_datetime = models.DateTimeField(auto_now=True)
-    article_likes = models.IntegerField(default=0)
-    article_dislikes = models.IntegerField(default=0)
+    article_title = models.CharField('Заголовок', max_length=200)
+    article_text = models.TextField('Текст')
+    article_datetime = models.DateTimeField('Дата и время')
+    article_likes = models.IntegerField('Количество лайков', default=0)
+    article_dislikes = models.IntegerField('Количество дизлайков', default=0)
 
     def __str__(self):
         text = self.article_title
         return text if len(text) <= 50 else text[:50]+'...'
+
 
 class Comments (models.Model):
     class Meta:
         db_table = 'comments'
 
     comments_article = models.ForeignKey(Article)
-    comments_text = models.CharField(max_length=200)
-    comments_likes = models.IntegerField(default=0)
-    comments_dislikes = models.IntegerField(default=0)
-    comments_datetime = models.DateTimeField(auto_now=True)
+    comments_text = models.CharField('Текст', max_length=200)
+    comments_likes = models.IntegerField('Количество лайков', default=0)
+    comments_dislikes = models.IntegerField('Количество дизлайков', default=0)
+    comments_datetime = models.DateTimeField('Дата и время')
 
     def __str__(self):
         text = self.comments_text
